3ea89c1a0c6e453fa9f8dfdd18b587c52f0bc847
Show Wai Yan <showwaiyan555@gmail.com>
2025-01-15 17:02:21 +0800

    Feature: finish beta version of minimax algo

--- script.js
+++ script.js
@@ -9,68 +9,98 @@ const gameBoard = [
 // false for Bot
 let turn = true;
 
+
+let bestMove = [];
+
 function gameSetup() {
-    for (const row in gameBoard) {
-        for (const column in row) gameBoard[row][column] = "";
+    for (let row=0; row<3; row++) {
+        for (let column=0; column<3; column++) gameBoard[row][column] = "";
     }
 }
 
 function minimax(gameBoard,depth,turn) {
     // Base case
-    if (gameFinish()) {
+    if (gameOver(checkCurrentPlayer(!turn),gameBoard)) {
+        //someone wins
+        if (checkCurrentPlayer(!turn) === User) {
+            return -1*countEmptySpace(gameBoard);
+        }
+        else {
+            return +1*countEmptySpace(gameBoard);
+        }
+    }
+    else if (gameFinish(gameBoard) || depth === 0) {
         // All space are failed
         return 0;
     }
-    else if (gameOver(checkCurrentPlayer())) {
-        //someone wins
-        if (checkCurrentPlayer() === User) return -1*countEmptySpace()+depth;
-        else return +1*countEmptySpace-depth;
-    }
 
     // Transerving
-
-    // return case
+    let bestScore = 0;
+    for (let row=0; row<3;row++) {
+        for (let column=0; column<3; column++) {
+            if (gameBoard[row][column] === "") {
+                if (checkCurrentPlayer(turn) === User) {
+                    User.move(gameBoard,row,column)
+                    let score = minimax(gameBoard,depth-1,!turn);
+                    if (score < bestScore) {
+                        bestScore = score;
+                        bestMove = [row,column];
+                    }
+                    gameBoard[row][column] = "";
+                } else {
+                    Bot.move(gameBoard,row,column);
+                    let score = minimax(gameBoard,depth-1,!turn);
+                    if (score > bestScore) {
+                        bestScore = score;
+                        bestMove = [row,column];
+                    }
+                    gameBoard[row][column] = "";
+                }
+            }
+        }
+    }
+    return bestScore;
 }
 
-function gameFinish() {
+function gameFinish(gameBoard) {
     let result = true;
-    for (const row in gameBoard) {
-        for (const column in row) {
+    for (let row=0; row<3; row++) {
+        for (let column=0; column<3; column++) {
             if (gameBoard[row][column] === "") result = false;
         }
     }
     return result;
 }
 
-function gameOver(player) {
+function gameOver(player,gameBoard) {
     const token = player.token;
 
     //Row check
     function  checkRow(row) {
-        return (gameBoard[row][0] === gameBoard[row][1] === gameBoard[row][2]);
+        return (gameBoard[row][0] === token && gameBoard[row][1] === token && gameBoard[row][2] === token);
     }
 
-    for (const row in gameBoard) if (checkRow(row)) return true;
+    for (let row=0; row<3; row++) if (checkRow(row)) return true;
 
     // Column check
     function checkColumn(column) {
-        return (gameBoard[0][column] === gameBoard[1][column] === gameBoard[2][column]);
+        return (gameBoard[0][column] === token && gameBoard[1][column] === token && gameBoard[2][column] === token);
     }
-    for (const column in gameBoard) if (checkColumn(column)) return true;
+    for (let column=0;column<3; column++) if (checkColumn(column)) return true;
 
     // Diagonal check
     function checkDiagonl() {
-        return (gameBoard[0][0] === gameBoard[1][1] === gameBoard[2][2]) || (gameBoard[0][2] === gameBoard[1][1] === gameBoard[2,0]);
+        return (gameBoard[0][0] === token && gameBoard[1][1] === token && gameBoard[2][2] === token) || (gameBoard[0][2] === token && gameBoard[1][1] === token && gameBoard[2,0] === token);
     }
     if (checkDiagonl()) return true;
 
     return false;
 }
 
-function countEmptySpace() {
+function countEmptySpace(gameBoard) {
     let count = 1;
-    for (const row in gameBoard) {
-        for (const column in row) if (gameBoard[row][column] === "") coutn++;;
+    for (let row=0; row<3; row++) {
+        for (let column=0; column<3; column++) if (gameBoard[row][column] === "") count++;;
     }
     return count;
 }
@@ -96,7 +126,7 @@ class Player {
         return this.#token;
     }
 
-    move(row,column) {
+    move(gameBoard,row,column) {
         gameBoard[row][column] = this.#token;
     }
 }
@@ -108,6 +138,8 @@ const column = document.querySelector("#column");
 const btn = document.getElementById("btn-go");
 
 btn.addEventListener('click',(e)=>{
-    User.move(+row.value, +column.value);
+    User.move(gameBoard,+row.value, +column.value);
+    minimax(structuredClone(gameBoard),100,!turn);
+    Bot.move(gameBoard,bestMove[0],bestMove[1]);
     console.table(gameBoard);
 })
